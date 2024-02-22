e08af1716c44518015d2f7047ff99a31fe71352d
olutayopeter <olutayopeter2014@gmail.com>
2024-02-22 12:43:25 +0100

    Update SimplePaymentGatewayApplication.java
    Update on Main Class

--- src/main/java/com/payment/simplepaymentgateway/SimplePaymentGatewayApplication.java
+++ src/main/java/com/payment/simplepaymentgateway/SimplePaymentGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableKafka
 public class SimplePaymentGatewayApplication {
 
 	public static void main(String[] args) {
